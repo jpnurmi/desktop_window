d3a4ad72dff3d1a35d9d3c0abf2f6c207af34198
J-P Nurmi <jpnurmi@gmail.com>
2020-12-27 11:36:59 +0100

    Windows: cleanup bogus getPlatformVersion

--- windows/desktop_window_plugin.cpp
+++ windows/desktop_window_plugin.cpp
@@ -3,9 +3,6 @@
 // This must be included before many other Windows headers.
 #include <windows.h>
 
-// For getPlatformVersion; remove unless needed for your plugin implementation.
-#include <VersionHelpers.h>
-
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
@@ -69,25 +66,7 @@ DesktopWindowPlugin::~DesktopWindowPlugin() {}
 void DesktopWindowPlugin::HandleMethodCall(
     const flutter::MethodCall<flutter::EncodableValue> &method_call,
     std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  // Replace "getPlatformVersion" check with your plugin's method.
-  // See:
-  // https://github.com/flutter/engine/tree/master/shell/platform/common/cpp/client_wrapper/include/flutter
-  // and
-  // https://github.com/flutter/engine/tree/master/shell/platform/glfw/client_wrapper/include/flutter
-  // for the relevant Flutter APIs.
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
-    }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-    
-  } else if (method_call.method_name().compare("getWindowSize") == 0) {
+  if (method_call.method_name().compare("getWindowSize") == 0) {
     HWND handle = GetActiveWindow();
     RECT rect;
 
